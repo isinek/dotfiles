d89e8e7ae84df840b3c6882c44154be1313fae15
Ivan Sinek <ivan.sinek@fonoa.com>
2026-08-14 18:19:52 +0200

    Updating current setup and adding presenterm

---
+++ presenterm/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eufCo pipefail
+
+TOOL="$(basename "$(dirname "$0")")"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SOURCE_DIR="${SCRIPT_DIR}"
+DESTINATION_DIR="${HOME}/.config/${TOOL}"
+
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/../lib/setup.sh"
+
+install_brew_formula_if_missing "${TOOL}" "${TOOL}"
+
+link_file \
+  "${SOURCE_DIR}/config.yaml" \
+  "${DESTINATION_DIR}/config.yaml"
